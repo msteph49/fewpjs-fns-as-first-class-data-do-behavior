bb8a685e964e0924229ce60559021798de126386
Michael Stephens <mdstephens26@yahoo.com>
2021-08-23 20:33:26 -0400

    commit tree db73a8df2e9e56c8d724e8737f3d4467e98ce433

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
+function greet(time){
+  const hour = parseInt(time)
+  if (hour < 12 ){
+    return "Good Morning"
+  } else if (hour >= 12 && hour < 17) {
+    return "Good Afternoon"
+  } else if (hour >= 17){
+    return "Good Evening"
+  }
+}
+
+function displayMessage(message){
+document.getElementById("greeting").textContent = message;
+}
+
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
